fff8eef974065816a9654a1f5eb2cbeae832d90d
kristinamaximyuk@gmail.com <kristinamaximyuk@gmail.com>
2025-10-22 11:26:46 +0000

    fixed reporting

--- models/marts/backend/core/bridge_ticket_segment.sql
+++ models/marts/backend/core/bridge_ticket_segment.sql
@@ -5,7 +5,3 @@ SELECT
     ticket_id, -- FK: Ticket ID
     segment_id -- FK: Master Segment ID
 FROM {{ ref('stg_backend__ticket_segment') }}
-
--- Ensure all keys exist in the master dimension
-INNER JOIN {{ ref('dim_segment') }} s
-    USING (segment_id)
