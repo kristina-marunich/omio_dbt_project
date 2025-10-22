45882afccb1da65a9193ce2ef3d1b7e16a6e304c
kristinamaximyuk@gmail.com <kristinamaximyuk@gmail.com>
2025-10-22 10:41:45 +0000

    fixed yaml, added incremental

--- models/marts/backend/core/fact_ticket_transaction.sql
+++ models/marts/backend/core/fact_ticket_transaction.sql
@@ -1,17 +1,36 @@
 -- models/marts/core/fact_ticket_transaction.sql
+{{
+    config(
+        materialized="incremental",
+        unique_key="ticket_id",
+        incremental_strategy="merge",
+        on_schema_change="fail",
+    )
+}}
 
 
-SELECT
-    ticket_id, -- PK: Ticket ID
-    booking_id, -- FK: Booking ID
-    ticket_issue_date_id, -- FK: Date ID
-    
+select
+    ticket_id,  -- PK: Ticket ID
+    booking_id,  -- FK: Booking ID
+    ticket_issue_date_id,  -- FK: Date ID
+
     -- Measures
     ticket_price_eur,
-    ticket_price AS ticket_price_original,
+    ticket_price as ticket_price_original,
     eur_rate,
-    
-    -- Contextual Dimensions
-    ticket_currency
 
-FROM {{ ref('int_ticket_base') }}
+    -- Contextual Dimensions
+    ticket_currency,
+    uploaded_at_timestamp
+
+from {{ ref("int_ticket_base") }}
+
+{% if is_incremental() %}
+
+    where
+        uploaded_at_timestamp >= (
+            select timestamp_sub(max(uploaded_at_timestamp), interval 7 day)
+            from {{ this }}
+        )
+
+{% endif %}
