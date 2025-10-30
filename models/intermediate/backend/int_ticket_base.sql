d38d1cfea0f6dcda5d0ab4d3ec902304f07eeb85
kristinamaximyuk@gmail.com <kristinamaximyuk@gmail.com>
2025-10-30 12:55:20 +0000

    changed materialisation ot incremental

--- models/intermediate/backend/int_ticket_base.sql
+++ models/intermediate/backend/int_ticket_base.sql
@@ -1,40 +1,46 @@
 -- models/intermediate/int_ticket_base.sql
-with
-    tickets_base as (
-        select
-            t.ticket_id as ticket_id,
-            t.booking_id as booking_id,
-            t.ticket_price,
-            t.ticket_currency,
 
-            date(b.created_at_timestamp) as transaction_date,
-            b.uploaded_at_timestamp,
-
-            cast(
-                format_date('%Y%m%d', date(b.created_at_timestamp)) as int
-            ) as ticket_issue_date_id
-
-        from {{ ref("stg_backend__ticket") }} t
-        inner join {{ ref("stg_backend__booking") }} b on t.booking_id = b.booking_id
-    ),
-
-    currency_rates as (
-        select date, from_currency, rate from {{ ref("stg_finance__currencies") }}
+{{
+    config(
+        materialized = "incremental",
+        incremental_strategy = "insert_overwrite",
+        partition_by = {"field": "uploaded_at_date", "data_type": "date"},
+        cluster_by = ["booking_id"],
+        on_schema_change = "fail"
     )
+}}
+
+with tickets as (
+    select *
+    from {{ ref("stg_backend__ticket") }}
+    {% if is_incremental() %}
+    where uploaded_at_date >= date_sub(current_date(), interval 7 day)
+    {% endif %}
+),
+bookings as (
+    select *
+    from {{ ref("stg_backend__booking") }}
+    {% if is_incremental() %}
+    where uploaded_at_date >= date_sub(current_date(), interval 7 day)
+    {% endif %}
+),
+currency_rates as (
+    select date, from_currency, rate
+    from {{ ref("stg_finance__currencies") }}
+)
 
 select
-    tb.ticket_id, --PK
-    tb.booking_id, -- FK
-    tb.ticket_issue_date_id,
-    tb.uploaded_at_timestamp,
-    tb.ticket_price,
-    tb.ticket_currency,
-
+    t.ticket_id,
+    t.booking_id,
+    cast(format_date('%Y%m%d', date(b.created_at_timestamp)) as int) as ticket_issue_date_id,
+    t.uploaded_at_timestamp,
+    t.uploaded_at_date,
+    t.ticket_price,
+    t.ticket_currency,
     coalesce(r.rate, 1.0) as eur_rate,
-    round(tb.ticket_price * coalesce(r.rate, 1.0), 2) as ticket_price_eur
-
-from tickets_base tb
-left join
-    currency_rates r
-    on upper(tb.ticket_currency) = upper(r.from_currency)
-    and tb.transaction_date = r.date
+    round(t.ticket_price * coalesce(r.rate, 1.0), 2) as ticket_price_eur
+from tickets t
+inner join bookings b on t.booking_id = b.booking_id
+left join currency_rates r
+    on upper(t.ticket_currency) = upper(r.from_currency)
+   and date(b.created_at_timestamp) = r.date
